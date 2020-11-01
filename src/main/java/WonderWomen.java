f533899f244f2116055eb9762505238d34c34027
Pabanthapa91 <aanayabthapa@gmail.com>
2020-11-01 14:50:07 -0500

    commit initial

---
+++ src/main/java/WonderWomen.java
@@ -0,0 +1,7 @@
+public class WonderWomen {
+    public static void main(String[] args) {
+
+
+        System.out.println("Rajmi is a Wonder WOmen");
+    }
+}
---
+++ src/test/java/SuperGirl.java
@@ -0,0 +1,7 @@
+public class SuperGirl {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hey ,, Rajmi is a super Girl");
+    }
+}
